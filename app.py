059e1edb9036038298486811189b51fa3604871a
Victor Sago <VictorSago01@gmail.com>
2022-06-29 16:32:02 +0200

    Stage 1 functionality complete

--- app.py
+++ app.py
@@ -21,19 +21,36 @@ def start():
 def delete_entry():
     now = datetime.datetime.now()
     current_date = [str(now.year), str(now.month), str(now.day)]
+    current_time = [str(now.hour), str(now.minute)]
+    if len(current_date[1]) < 2:
+        current_date[1] = '0' + current_date[1]
+    if len(current_date[2]) < 2:
+        current_date[2] = '0' + current_date[2]
     error = None
     meth = request.method
-    ''' if request.method == "DELETE":
-        name = request.form["name"]
-        for item in dum:
-            if item[0].lower() == name.lower():
-                dum.remove(item)
-                break
-        return render_template('delete.html', list=dum, date=current_date, deleted=name, error=error) '''
     name = request.args["name"]
+    deleted = False
     for item in dum:
         if item[0].lower() == name.lower():
             dum.remove(item)
+            deleted = True
             break
-    return render_template('delete.html', date=current_date, deleted=name, error=error, method=meth)
-    #return render_template('list.html', list=dum, date=current_date)
+    if not deleted:
+        name = None
+    return render_template('delete.html', date=current_date, time=current_time, deleted_name=name, error=error, method=meth)
+
+@app.route("/insert")
+def insert_entry():
+    now = datetime.datetime.now()
+    current_date = [str(now.year), str(now.month), str(now.day)]
+    current_time = [str(now.hour), str(now.minute)]
+    if len(current_date[1]) < 2:
+        current_date[1] = '0' + current_date[1]
+    if len(current_date[2]) < 2:
+        current_date[2] = '0' + current_date[2]
+    error = None
+    name = request.args["name"]
+    phone = request.args["phone"]
+    dum.append((name, phone))
+    return render_template('insert.html', date=current_date, time=current_time, inserted_name=name, inserted_phone=phone, error=error)
+ 
